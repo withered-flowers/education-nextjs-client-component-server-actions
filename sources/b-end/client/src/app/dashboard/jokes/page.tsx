1cb7c3c8cab53bc8681dcdcbe1206d28016bc7c4
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-11-14 17:34:36 +0700

    feat: copy initial to b-end

---
+++ sources/b-end/client/src/app/dashboard/jokes/page.tsx
@@ -0,0 +1,48 @@
+// Membuat definition type untuk data yang akan di-parse
+type Joke = {
+  id: number;
+  setup: string;
+  delivery: string;
+};
+
+const fetchJokes = async () => {
+  const response = await fetch("http://localhost:3001/joke");
+  const responseJson: Joke[] = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Waduh Error ...");
+  }
+
+  return responseJson;
+};
+
+const DashboardJokePage = async () => {
+  const jokes = await fetchJokes();
+
+  return (
+    <section>
+      <h2 className="text-2xl font-semibold">Dashboard Page - Jokes</h2>
+
+      <table className="mt-4">
+        <thead>
+          <tr>
+            <th className="p-4">No</th>
+            <th className="p-4">Setup</th>
+            <th className="p-4">Delivery</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jokes.map((todo, idx) => (
+            <tr key={todo.id}>
+              <td>{idx + 1}</td>
+              <td>{todo.setup}</td>
+              <td>{todo.delivery}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default DashboardJokePage;
